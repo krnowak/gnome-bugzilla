ca85b1f0dfeaaf02cbe99a95c472ede2c20ec3ab
Krzesimir Nowak <qdlacz@gmail.com>
2014-09-29 22:56:20 +0200

    Add an OldStatus extension
    It is a quick and dirty extension to simulate the actual current GNOME
    schema for migration needs.

---
+++ extensions/OldStatus/Extension.pm
@@ -0,0 +1,142 @@
+# -*- Mode: perl; indent-tabs-mode: nil -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# This Source Code Form is "Incompatible With Secondary Licenses", as
+# defined by the Mozilla Public License, v. 2.0.
+
+package Bugzilla::Extension::OldStatus;
+use strict;
+use warnings;
+use base qw(Bugzilla::Extension);
+
+# The code for this is in ./extensions/OldStatus/lib/*.pm
+use Bugzilla::Extension::OldStatus::Util;
+
+use List::MoreUtils qw(any);
+
+our $VERSION = '0.01';
+
+# See the documentation of Bugzilla::Hook ("perldoc Bugzilla::Hook"
+# in the bugzilla directory) for a list of all available hooks.
+
+sub install_update_db {
+    my $dbh = Bugzilla->dbh;
+    my $column = $dbh->bz_column_info(a(), 'status');
+
+    unless (defined ($column))
+    {
+        $dbh->bz_start_transaction;
+
+        my $insert = $dbh->prepare('INSERT INTO ' . a_s() . ' (value, sortkey, description) VALUES (?,?,?)');
+        my $sortorder = 0;
+        my @pairs = (['none', 'an unreviewed patch'],
+                     ['accepted-commit_now', 'The maintainer has given commit permission'],
+                     ['needs-work', 'The patch needs work'],
+                     ['accepted-commit_after_freeze', 'This patch is acceptable, but can\'t be committed until after the relevant freeze (string, etc.) is lifted.'],
+                     ['commited', 'This patch has already been committed but the bug remains open for other reasons'],
+                     ['rejected', 'The patch provides a change that is just not wanted, or the patch can\'t be fixed to be correct without rewriting.  Maintainers should always explain their reasons whenever marking a patch as rejected.'],
+                     ['reviewed', 'None of the other states made sense or are quite correct, but the other comments in the bug explain the status of the patch and the patch should be considered to have been reviewed.  If the submitter doesn\'t feel the comments on the patch are clear enough, they can unset this state']);
+        foreach my $pair (@pairs) {
+            $sortorder += 100;
+            $insert->execute($pair->[0], $sortorder, $pair->[1]);
+        }
+
+        my $field_params = {
+            name => a_st(),
+            description => 'Attachment status',
+            type => Bugzilla::Constants::FIELD_TYPE_SINGLE_SELECT
+        };
+        Bugzilla::Field->create($field_params);
+
+        my $definition = {
+            'NOTNULL' => 1,
+            'REFERENCES' => {
+                'COLUMN' => 'value',
+                'DELETE' => 'CASCADE',
+                'TABLE' => 'attachment_status'
+            },
+                    'TYPE' => 'varchar(64)'
+        };
+        $dbh->bz_add_column(a(), st(), $definition, 'none');
+        $dbh->bz_add_index(a(), a() . '_index',
+                       {FIELDS => ['ispatch'], TYPE => ''});
+        $dbh->bz_add_index(a(), a_s(),
+                       {FIELDS => [st()], TYPE => ''});
+
+        $dbh->bz_commit_transaction;
+    }
+}
+
+sub db_schema_abstract_schema {
+    my ($self, $args) = @_;
+    my $schema = $args->{'schema'};
+
+    unless (exists ($schema->{a_s()}))
+    {
+        my $definition = {
+              'FIELDS' => [
+                            'id',
+                            {
+                              'NOTNULL' => 1,
+                              'PRIMARYKEY' => 1,
+                              'TYPE' => 'SMALLSERIAL'
+                            },
+                            'value',
+                            {
+                              'NOTNULL' => 1,
+                              'TYPE' => 'varchar(64)'
+                            },
+                            'sortkey',
+                            {
+                              'DEFAULT' => 0,
+                              'NOTNULL' => 1,
+                              'TYPE' => 'INT2'
+                            },
+                            'isactive',
+                            {
+                              'DEFAULT' => 'TRUE',
+                              'NOTNULL' => 1,
+                              'TYPE' => 'BOOLEAN'
+                            },
+                            'visibility_value_id',
+                            {
+                              'TYPE' => 'INT2'
+                            },
+                            'description',
+                            {
+                              'NOTNULL' => 1,
+                              'TYPE' => 'MEDIUMTEXT'
+                            }
+                          ],
+              'INDEXES' => [
+                             'attachment_status_value_idx',
+                             {
+                               'FIELDS' => [
+                                             'value'
+                                           ],
+                               'TYPE' => 'UNIQUE'
+                             },
+                             'attachment_status_sortkey_idx',
+                             [
+                               'sortkey',
+                               'value'
+                             ],
+                             'attachment_status_visibility_value_id_idx',
+                             [
+                               'visibility_value_id'
+                             ]
+                           ]
+        };
+
+        $schema->{a_s()} = $definition;
+    }
+}
+
+sub enabled {
+    1;
+}
+
+__PACKAGE__->NAME;
---
+++ extensions/OldStatus/lib/Util.pm
@@ -0,0 +1,104 @@
+# -*- Mode: perl; indent-tabs-mode: nil -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# This Source Code Form is "Incompatible With Secondary Licenses", as
+# defined by the Mozilla Public License, v. 2.0.
+
+package Bugzilla::Extension::OldStatus::Util;
+use strict;
+use warnings;
+use base qw(Exporter);
+use Bugzilla::Constants;
+use Data::Dumper;
+
+our @EXPORT = qw(
+    st
+    a_s
+    a
+    a_st
+    as_dbg
+);
+
+# This file can be loaded by your extension via
+# "use Bugzilla::Extension::AttachmentStatus::Util". You can put functions
+# used by your extension in here. (Make sure you also list them in
+# @EXPORT.)
+
+sub st {
+    'status';
+}
+
+sub a_s {
+    'attachment_' . st ();
+}
+
+sub a {
+    'attachments'
+}
+
+sub a_st {
+    a() . '.' . st();
+}
+
+sub _prepare_msg {
+    my $raw_msg = '';
+    local $Data::Dumper::Terse = 1;
+    my $dumper_used = 0;
+    my $spacing = '';
+    my @copy = @_;
+
+    if ((@copy > 0) && defined ($copy[0]) && (ref($copy[0]) eq '') && ($copy[0] =~ /^(\s+)/)) {
+        $spacing = $1;
+        $copy[0] =~ s/^\s+//;
+    }
+
+    foreach (@copy) {
+        $_ = '<UNDEF>' unless defined;
+
+        if (ref eq '') {
+            my $s = $_;
+            if ($dumper_used) {
+                $dumper_used = 0;
+                $s =~ s/^,?\s*//;
+            }
+            $raw_msg .= $s;
+        } else {
+            unless ($raw_msg =~ /\n$/) {
+                $raw_msg =~ s/\s+$//;
+                $raw_msg .= "\n";
+            }
+            $raw_msg .= Dumper($_);
+            $dumper_used = 1;
+        }
+    }
+    my $final_msg = '';
+    my @splitted = split("\n", $raw_msg);
+    if (@splitted > 1) {
+        my $sep = '========';
+        unshift(@splitted, $sep);
+        push(@splitted, $sep);
+    }
+    foreach (@splitted) {
+        next if $_ eq '';
+        $final_msg .= 'Old attachment status: ' . $spacing . $_ . "\n";
+    }
+
+    return $final_msg;
+}
+
+sub as_dbg {
+    my $datadir = bz_locations()->{'datadir'};
+    my $mesg = _prepare_msg(@_);
+    if (-w "$datadir/errorlog") {
+        open(ERRORLOGFID, ">>$datadir/errorlog");
+        print ERRORLOGFID $mesg;
+        close ERRORLOGFID;
+    } else {
+        die $mesg;
+    }
+}
+
+1;
