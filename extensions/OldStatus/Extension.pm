a0ba0746ef0eaf425a8b674b163574572a74f17f
Krzesimir Nowak <qdlacz@gmail.com>
2014-10-03 21:52:22 +0200

    Simplify index addition

--- extensions/OldStatus/Extension.pm
+++ extensions/OldStatus/Extension.pm
@@ -139,10 +139,8 @@ sub install_before_final_checks
 
     $dbh->bz_start_transaction;
     $dbh->bz_add_column(a(), st(), $definition, 'none');
-    $dbh->bz_add_index(a(), a() . '_index',
-                   {FIELDS => ['ispatch'], TYPE => ''});
-    $dbh->bz_add_index(a(), a_s(),
-                   {FIELDS => [st()], TYPE => ''});
+    $dbh->bz_add_index(a(), 'attachment_index', ['ispatch']);
+    $dbh->bz_add_index(a(), a_s(), [st()]);
     $dbh->bz_commit_transaction;
 }
 
