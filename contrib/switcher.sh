02b14e09515286b82d2d483e5baa461e14d59edc
Krzesimir Nowak <qdlacz@gmail.com>
2014-10-05 17:13:20 +0200

    Fix a leftover from renaming

--- contrib/switcher.sh
+++ contrib/switcher.sh
@@ -3,11 +3,11 @@
 if [ "${1}" = 'old' ]
 then
     rm -f extensions/OldStatus/disabled
-    touch extensions/AttachmentStatus/disabled
+    touch extensions/GnomeAttachmentStatus/disabled
 elif [ "${1}" = 'new' ]
 then
     touch extensions/OldStatus/disabled
-    rm -f extensions/AttachmentStatus/disabled
+    rm -f extensions/GnomeAttachmentStatus/disabled
 else
     echo 'Wrong parameter, expected either "new" or "old"'
     exit 1
