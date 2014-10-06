03f7e6531d8eadfed3b30b0cf81e311e4ae3bce5
Krzesimir Nowak <qdlacz@gmail.com>
2014-10-06 21:48:24 +0200

    Try using hidden input for nonpatches

--- extensions/GnomeAttachmentStatus/web/gnome_attachment_status.js
+++ extensions/GnomeAttachmentStatus/web/gnome_attachment_status.js
@@ -1,15 +1,24 @@
-function show_hide_gnome_attachment_status(evt) {
-    var combo = document.getElementById('attachment_gnome_attachment_status');
+function showOrHideGnomeAttachmentStatus() {
+    var combo = document.getElementById('attachment_gnome_attachment_status_combo');
+    var hidden = document.getElementById('attachment_gnome_attachment_status_hidden');
+    var ispatch = document.getElementById('ispatch');
 
-    if (document.getElementById('ispatch').checked){
-        YAHOO.util.Dom.replaceClass(combo, 'gnome_attachment_status_is_not_a_patch', 'gnome_attachment_status_is_a_patch');
-    }else{
-        YAHOO.util.Dom.replaceClass(combo, 'gnome_attachment_status_is_a_patch', 'gnome_attachment_status_is_not_a_patch');
+    if (combo !== null && hidden !== null){
+	if (ispatch.checked) {
+            YAHOO.util.Dom.replaceClass(combo, 'gnome_attachment_status_is_not_a_patch', 'gnome_attachment_status_is_a_patch');
+            YAHOO.util.Dom.replaceClass(hidden, 'gnome_attachment_status_is_a_patch', 'gnome_attachment_status_is_not_a_patch');
+	} else {
+            YAHOO.util.Dom.replaceClass(combo, 'gnome_attachment_status_is_a_patch', 'gnome_attachment_status_is_not_a_patch');
+            YAHOO.util.Dom.replaceClass(hidden, 'gnome_attachment_status_is_not_a_patch', 'gnome_attachment_status_is_a_patch');
+	}
     }
 
     1;
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('ispatch').addEventListener('change', show_hide_gnome_attachment_status);
+    ispatch = document.getElementById('ispatch');
+    if (ispatch !== null) {
+	ispatch.addEventListener('change', showOrHideGnomeAttachmentStatus);
+    }
 });
